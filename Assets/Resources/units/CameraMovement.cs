7e82f68573d49f91cf0a43ef4bb3183a0cd53f88
yannick <tediyannick78@gmail.com>
2024-03-04 16:47:15 +0100

    v2
    -ajustement des déplacements de la camera.
    # déplacement lorsque la souris atteint certaine zone de l'écran
    
    -ajout compteur de fps

--- Assets/Resources/units/CameraMovement.cs
+++ Assets/Resources/units/CameraMovement.cs
@@ -2,79 +2,39 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float scrollSpeed = 5f; // Vitesse de défilement de la molette de la souris et du zoom par pincement
-    public float dragSpeed = 20f; // Vitesse de déplacement de la caméra lors du glissement à la souris ou au doigt
-
-    private Vector3 dragOrigin;
-    private float initialTouchDistance;
-    private Vector3 initialCameraPosition;
-    private bool isDragging = false;
+    public float scrollSpeed = 5f; // Vitesse de défilement de la molette de la souris
+    public float edgeScrollSpeed = 20f; // Vitesse de déplacement de la caméra lorsque le pointeur atteint le bord de l'écran
+    public float edgeScrollZoneSize = 500f; // Taille de la zone sensible aux bords de l'écran
 
     void Update()
     {
         // Contrôle de défilement de la molette de la souris
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        // Ajustez ici si vous voulez que le zoom avant/arrière soit influencé par l'angle
         transform.Translate(0, 0, scroll * scrollSpeed, Space.Self);
 
-        // Contrôle de déplacement de la caméra avec le clic de la souris ou le toucher
-        if (Input.GetMouseButtonDown(2) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began))
+        // Contrôle de déplacement de la caméra lorsque le pointeur atteint le bord de l'écran
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 move = Vector3.zero;
+
+        if (mousePosition.x < edgeScrollZoneSize)
         {
-            if (Input.touchCount == 1)
-            {
-                dragOrigin = Input.GetTouch(0).position;
-            }
-            else
-            {
-                dragOrigin = Input.mousePosition;
-            }
-            isDragging = true;
+            move += Vector3.left * edgeScrollSpeed * Time.deltaTime;
+        }
+        else if (mousePosition.x > Screen.width - edgeScrollZoneSize)
+        {
+            move += Vector3.right * edgeScrollSpeed * Time.deltaTime;
         }
 
-        if (isDragging && (Input.GetMouseButton(2) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)))
+        if (mousePosition.y < edgeScrollZoneSize)
         {
-            Vector3 currentPos;
-            if (Input.touchCount == 1)
-            {
-                currentPos = Input.GetTouch(0).position;
-            }
-            else
-            {
-                currentPos = Input.mousePosition;
-            }
-
-            Vector3 pos = Camera.main.ScreenToViewportPoint(currentPos - dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-
-            // Ajustement pour tenir compte de la rotation de la caméra
-            Vector3 forward = Quaternion.Euler(0, transform.eulerAngles.y, 0) * Vector3.forward;
-            Vector3 right = Quaternion.Euler(0, transform.eulerAngles.y, 0) * Vector3.right;
-            move = pos.x * dragSpeed * right + pos.y * dragSpeed * forward;
-
-            // Appliquer le mouvement
-            transform.Translate(-move, Space.World);
+            move += Vector3.back * edgeScrollSpeed * Time.deltaTime;
+        }
+        else if (mousePosition.y > Screen.height - edgeScrollZoneSize)
+        {
+            move += Vector3.forward * edgeScrollSpeed * Time.deltaTime;
         }
 
-        if (Input.GetMouseButtonUp(2) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended))
-        {
-            isDragging = false;
-        }
-
-        // Contrôle de zoom par pincement
-        if (Input.touchCount == 2)
-        {
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
-
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-            transform.Translate(0, 0, deltaMagnitudeDiff * scrollSpeed * Time.deltaTime, Space.Self);
-        }
+        // Appliquer le mouvement
+        transform.Translate(move, Space.World);
     }
 }
