3a74f16af3b2eb92540bba9da177ed73a51eeb9d
yannick <tediyannick78@gmail.com>
2024-03-15 00:00:52 +0100

    v6
    -ajout placement des bâtiments (caserne)
    -création d'un layer "build" (permet d'ignorer les bâtiments lors de lancé de rayon )

--- Assets/Resources/ui/buttonCaserne.cs
+++ Assets/Resources/ui/buttonCaserne.cs
@@ -1,28 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonQuit : MonoBehaviour
+public class ButtonCaserne : MonoBehaviour
 {
-    
+    public GameObject caserne;
+    public static bool isPlacing = false;
     public GameObject buildingMenu;
-
     public CameraController CameraController;
 
-  
 
     void Start()
     {
-        
         Button button = GetComponent<Button>();
-
-        
         button.onClick.AddListener(OnClick);
     }
 
-    
     void OnClick()
     {
-        
+        isPlacing = true;
         buildingMenu.SetActive(false);
         CameraController.EnabledCameraController();
     }
